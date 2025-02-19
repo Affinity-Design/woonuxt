e85cd9b1c92e5b67e790461e7b7d97516c2212e7
RarifyApps <paul@affinitydesign.ca>
2025-02-18 23:55:32 -0500

    Fix Rounding

--- components/shopElements/StripeElement.vue
+++ components/shopElements/StripeElement.vue
@@ -2,20 +2,24 @@
 const { cart } = useCart();
 const { stripe } = defineProps(["stripe"]);
 
-const rawCartTotal = computed(
-  () => cart.value && parseFloat(cart.value.rawTotal as string) * 100
-);
+const rawCartTotal = computed(() => {
+  console.log("cart value", cart.value.rawTotal);
+  return (
+    cart.value && Math.round(parseFloat(cart.value.rawTotal as string) * 100)
+  );
+});
 const emit = defineEmits(["updateElement"]);
 let elements = null as any;
 
 const options = {
   mode: "payment",
-  currency: "eur",
+  currency: "cad",
   amount: rawCartTotal.value,
   // paymentMethodCreation: 'manual',
 };
 
 const createStripeElements = async () => {
+  console.log("options", options);
   elements = stripe.elements(options);
   const paymentElement = elements.create("card", { hidePostalCode: true });
   paymentElement.mount("#card-element");
